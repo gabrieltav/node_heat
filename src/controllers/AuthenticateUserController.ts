00d1eddefffb22075fdbc1adedd5d5688a5ce0bc
Gabriel Tavares <tavaresgabriel963@gmail.com>
2021-10-21 19:45:25 -0400

    final project

--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -2,17 +2,17 @@ import { Request, Response } from "express";
 import { AuthenticateUserService } from "../services/AuthenticateUserService";
 
 class AuthenticateUserController {
-  async handle(request: Request, response: Response) {
-    const { code } = request.body;
+    async handle(request: Request, response: Response) {
+        const { code } = request.body;
 
-    const service = new AuthenticateUserService();
-    try {
-      const result = await service.execute(code);
-      return response.json(result);
-    } catch (err) {
-      return response.send({ error: "Request failed with status code 401" });
+        const service = new AuthenticateUserService();
+        try {
+            const result = await service.execute(code);
+            return response.json(result);
+        } catch (err) {
+            return response.send({ error: "Request failed with status code 401" });
+        }
     }
-  }
 }
 
 export { AuthenticateUserController };
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import { serverHttp } from "./app";
+
+serverHttp.listen(4000, () => {
+    console.log("server is running on PORT 4000");
+});
